7ea4e2bfe8a1dd1e31b21bb99f37dbfeb4d8d8d1
Sed9yDataBank <benzidsedki@gmail.com>
2020-04-24 14:15:44 +0100

    Models And Services For Data

---
+++ src/app/models/country-data.model.ts
@@ -0,0 +1,13 @@
+export default class CountryDataModel {
+  public country: string;
+  public cases: number;
+  public todayCases: number;
+  public deaths: number;
+  public todayDeaths: number;
+  public recovered: number;
+  public active: number;
+  public critical: number;
+  public casesPerOneMillion: number;
+  public flag: string;
+  public region: string;
+}
